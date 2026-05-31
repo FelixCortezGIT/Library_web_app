534b51a06c66da72c0f7cb7734aed22de5419136
TomasMacula <tomasmacula@live.com>
2026-05-31 16:23:44 +0200

    prihlasenie

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ load_dotenv()
 print(os.getenv("DATABASE_URL"))
 
 app = Flask(__name__)
-# app.config["SSQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://neondb_owner:npg_J4KYfHNULD5O@ep-floral-dream-alrhxd2c-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 class Author(db.Model):
