cc680f8eceab69865768b5c2d0aa62647406640b
TomasMacula <tomasmacula@live.com>
2026-05-31 15:12:30 +0200

    .env .gitignore requirements.txt app.py - pripojenie a test

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-# flask
-# psycopg2-binary
-# python-dotenv
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+def get_db_connection():
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    return conn
+
+conn = get_db_connection()
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM authors WHERE author_id = 1;")
+rows = cursor.fetchall()
+print(rows)
+cursor.close()
+conn.close()
